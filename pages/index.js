1907b6ddb59ad09fab7c9d78e2820fd770f66301
Blenda Fröjdh <blendaf@kth.se>
2020-03-07 08:24:40 +0100

    Initialize repo

---
+++ pages/index.js
@@ -0,0 +1,17 @@
+import Link from 'next/Link'
+import styled from 'styled-components'
+
+const Title = styled.div`
+  color: green;
+`
+
+const App = () => (
+  <div>
+    <Title>Hello World</Title>
+    <Link href="/start">
+      <a>Start Page</a>
+    </Link>
+  </div>
+)
+
+export default App
---
+++ pages/start.js
@@ -0,0 +1,11 @@
+import Link from 'next/Link'
+
+const start = () => (
+  <div>
+    <h1>hejsan</h1>
+    <Link href="/">
+      <div>Go back to index</div>
+    </Link>
+  </div>
+)
+export default start
