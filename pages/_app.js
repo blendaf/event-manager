536b46356f1d50f850767149c689d07551665392
Blenda Fröjdh <blendaf@kth.se>
2020-03-20 21:34:19 +0100

    styled components and themeprovider added

---
+++ pages/_app.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import NextApp from 'next/app'
+import { ThemeProvider } from 'styled-components'
+import theme from '../utils/theme'
+
+export default class App extends NextApp {
+  render() {
+    const { Component, pageProps } = this.props
+    return (
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    )
+  }
+}
--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,30 @@
 import Link from 'next/link'
 import styled from 'styled-components'
+import React, { useEffect, useState, useRef } from 'react'
+import { createGlobalStyle, ThemeProvider } from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+  body {
+   background-color: ${props => props.theme.colors.selection};
+   margin: 0;
+  }
+`
+
+const Background = styled.div`
+  background-color: white;
+  width: 300px;
+  height: 300px;
+`
 
 const Title = styled.div`
   color: green;
 `
 
-const App = () => (
-  <div>
-    <Title>Hello World</Title>
-    <Link href="/start">
-      <a>Start Page</a>
-    </Link>
-  </div>
+export default () => (
+  <React.Fragment>
+    <Background>
+      <Title>Hejans</Title>
+    </Background>
+    <GlobalStyle />
+  </React.Fragment>
 )
-
-export default App
